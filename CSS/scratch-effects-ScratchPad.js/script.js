d0a0bc70546f49a6b26a2b5002051ba2458d0a9f
Mridul2820 <immridulpanda2820@gmail.com>
2020-04-30 12:14:12 +0530

    Scratch Effects ScratchPad.js

---
+++ CSS/scratch-effects-ScratchPad.js/script.js
@@ -0,0 +1,5 @@
+$('#scratch').wScratchPad({
+    size        : 100,          // The size of the brush/scratch.
+    bg          : 'assets/full_puja.jpg',  // Background (image path or hex color).
+    fg          : '#000',  // Foreground (image path or hex color).
+  });
