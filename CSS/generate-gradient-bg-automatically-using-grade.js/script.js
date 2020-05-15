fa06066f20e8911d495bddf35b84faa635569b04
Mridul2820 <immridulpanda2820@gmail.com>
2020-05-15 17:46:07 +0530

    Generate Gradient BG Automatically using Grade Js

---
+++ CSS/generate-gradient-bg-automatically-using-grade.js/script.js
@@ -0,0 +1,8 @@
+window.addEventListener('load', function(){
+    /*
+        A NodeList of all your image containers (Or a single Node).
+        The library will locate an <img /> within each
+        container to create the gradient from.
+     */
+    Grade(document.querySelectorAll('.box'))
+})
